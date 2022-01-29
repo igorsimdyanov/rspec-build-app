4a483ca2b38dbf9940080755a05426a5b0dcb97a
Igor Simdyanov <igorsimdyanov@gmail.com>
2022-01-29 20:55:32 +0300

    VII.2 Колбэки, хэлперы и let

--- spec/services/collection_generator_spec.rb
+++ spec/services/collection_generator_spec.rb
@@ -2,16 +2,18 @@ require_relative '../../lib/services/collection_generator'
 
 RSpec.describe Services::CollectionGenerator do
   describe 'успешно выполняет' do
-    it 'генерацию массива из диапазона' do
-      expect(Services::CollectionGenerator.call(1, 10)).to eq [*1..10]
-    end
+    let(:array_of_ten_elements) { Services::CollectionGenerator.call(1, 10) }
 
     it 'генерацию массива из диапазона' do
-      expect(Services::CollectionGenerator.call(1, 10)).to include 5
+      expect(array_of_ten_elements).to eq [*1..10]
+    end
+
+    it 'генерацию массива и число 5 в него входит' do
+      expect(array_of_ten_elements).to include 5
     end
 
     it 'генерацию массива и число 100 в него не входит' do
-      expect(Services::CollectionGenerator.call(1, 10)).not_to include 100
+      expect(array_of_ten_elements).not_to include 100
     end
   end
 
