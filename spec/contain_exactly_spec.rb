28ff75b060cbd1645b97a638472681a3c842ec07
Igor Simdyanov <igorsimdyanov@gmail.com>
2022-02-14 21:16:09 +0300

    VII.4 Матчеры высшего порядка

---
+++ spec/contain_exactly_spec.rb
@@ -0,0 +1,17 @@
+RSpec.describe 'Матчер contain_exactly' do
+  let (:products) do
+    [
+      { name: 'Процессор', price: 40_399 },
+      { name: 'Материнская плата', price: 16_999 },
+      { name: 'Видеокарта', price: 55_999 }
+    ]
+  end
+  
+  it 'успешно работает' do
+    expect(products).to contain_exactly(
+      { name: 'Видеокарта', price: a_value < 60_000 },
+      { name: 'Материнская плата', price: a_value_between(10_000, 20_000) },
+      { name: 'Процессор', price: a_value > 40_000 },
+    )
+  end
+end
